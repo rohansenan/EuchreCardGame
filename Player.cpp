b50fbebbd8d32eaa577a1cfb33281af4d15e20a7
rsenan <rsenan@umich.edu>
2022-02-17 19:38:45 -0500

    hello

--- Player.cpp
+++ Player.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
+#include "Player.h"
+#include "Pack.h"
+#include "Card.h"
 
 using namespace std;
+
+Player * Player_factory(const std::string &name, const std::string &strategy)
+{
+    if (strategy == "simple")
+    {
+        
+    }
+
+}
